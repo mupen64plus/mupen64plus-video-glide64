1899d7090d330d031555487b203098137fc93a41
Sven Eckelmann <sven.eckelmann@gmx.de>
2010-03-10 18:17:29 +0100

    Add missing copyright information to m64p.h

--- src/m64p.h
+++ src/m64p.h
@@ -1,3 +1,24 @@
+/******************************************************************************
+ * Glide64 - Glide video plugin for Nintendo 64 emulators.
+ * http://bitbucket.org/wahrhaft/mupen64plus-video-glide64/
+ *
+ * Copyright (C) 2009 Jon Ring
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *****************************************************************************/
+
 #ifndef M64P_H
 #define M64P_H
 
